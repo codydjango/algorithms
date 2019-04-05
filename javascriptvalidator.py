04734ddb08ad1c01c1c0e9717afddd26c4b1fb2f
Cody Django Redmond <codydjango@gmail.com>
2019-04-05 15:31:40 -0700

    javascriptvalidator

---
+++ javascriptvalidator.py
@@ -0,0 +1,56 @@
+#! /usr/bin/env python3
+import unittest
+
+
+def javascriptvalidator(string):
+    openers = set(["{", "(", "["])
+    closers = set(["}", ")", "]"])
+
+    open_to_close = {
+        "{": "}",
+        "[": "]",
+        "(": ")",
+        "}": "{",
+        "]": "[",
+        ")": "(",
+    }
+
+    stack = []
+
+    for char in string:
+        if char in openers:
+            stack.append(open_to_close[char])
+        elif char in closers:
+            try:
+                if stack.pop() != char:
+                    return 0
+            except IndexError:
+                return 0
+        else:
+            pass
+    return 1
+
+
+class Test(unittest.TestCase):
+    def test_passes_easy(self):
+        actual = javascriptvalidator("[{[]}]")
+        expected = 1
+        self.assertEqual(actual, expected)
+    
+    def test_passes_hard(self):
+        actual = javascriptvalidator("""[{foo: "bar", baz: [{bat:[1,2]}, 3, 4]}]""")
+        expected = 1
+        self.assertEqual(actual, expected)
+    
+    def test_fails_easy(self):
+        actual = javascriptvalidator("[]}")
+        expected = 0
+        self.assertEqual(actual, expected)
+
+    def test_fails_hard(self):
+        actual = javascriptvalidator("""{foo: "bar", baz: [{bat:[1,2]}, 3, 4]}]""")
+        expected = 0
+        self.assertEqual(actual, expected)
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
